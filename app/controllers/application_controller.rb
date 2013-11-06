f1208081bb098983e11698fc0bf728c2009a8dad
Kenny Chandrasekera <kchandrasekera@gmail.com>
2013-11-06 15:55:29 -0800

    Added demo account and redirection after sign-out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,8 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(user)
     user_url(user)
   end
+  
+  def after_sign_out_path_for(user)
+    new_user_session_url
+  end
 end
