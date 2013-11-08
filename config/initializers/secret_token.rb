99639cfa294a93e1a8bb5fb4116c2d2ab9728f5c
Kenny Chandrasekera <kchandrasekera@gmail.com>
2013-11-07 22:12:08 -0800

    Obscured secrets with figaro

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-TrelloClone::Application.config.secret_token = '4555c17fee3eb85ecbd3f05655c8f29f4cdcb5107067e8c418d7454ce1a5b00e85425c901d810159b9f8687cf6f420514e605dbd539d81bbcbd30c486c0dc6ec'
+TrelloClone::Application.config.secret_token = ENV["CHECKLO_SECRET_TOKEN"]
