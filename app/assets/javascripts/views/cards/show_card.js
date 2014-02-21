733fe5ff947c5fd936b0c71867e4740339fd82bf
Kenny Chandrasekera <kchandrasekera@gmail.com>
2014-02-21 14:51:30 -0800

    Hide card modals before persisting data

--- app/assets/javascripts/views/cards/show_card.js
+++ app/assets/javascripts/views/cards/show_card.js
@@ -1,13 +1,13 @@
 TrelloClone.Views.ShowCard = Backbone.View.extend({
   template: JST["cards/show"],
-  
+
   initialize: function() {
     var that = this;
     this.listenTo(this.model, 'change', function () {
       setTimeout(this.render.bind(that), 350);
     });
   },
-  
+
   events: {
     "click .card": "showModal",
     "click .card-save": "saveChanges",
@@ -15,19 +15,19 @@ TrelloClone.Views.ShowCard = Backbone.View.extend({
     "card-reorder": "cardReorder",
     "get-card": "getCard"
   },
-  
+
   render: function() {
     renderedContent = this.template({
       card: this.model
     });
-    
+
     this.$el.html(renderedContent);
     if (this.model.get("completed")) {
       this.$el.find(".card").addClass("completed");
     }
     return this;
   },
-  
+
   showModal: function(event) {
     var modalTitle = this.model.get("card_name");
     var modalComment = this.model.get("comment");
@@ -36,28 +36,30 @@ TrelloClone.Views.ShowCard = Backbone.View.extend({
     this.$el.find(".modal-comment").html(modalComment);
     this.$el.find(".modal-completed").prop('checked', modalCompleted);
   },
-  
+
   saveChanges: function(event) {
     var cardName = this.$el.find(".modal-title").val();
     var cardComment = this.$el.find(".modal-comment").val();
     var cardCompleted = this.$el.find(".modal-completed:checked").val();
     cardCompleted = ((cardCompleted === "on") ? true : false);
     var changes = {card_name: cardName, comment: cardComment, completed: cardCompleted};
-    
+
     this.model.set(changes);
-    
-    cardView = this;
-    this.model.save(changes, {
-      success: function() {
-        $("#cardModal-" + cardView.model.id).modal("hide");    
-      }
-    });
+    $("#cardModal-" + this.model.id).modal("hide");
+    this.model.save();
+
+    // cardView = this;
+    // this.model.save(changes, {
+    //   success: function() {
+    //     $("#cardModal-" + cardView.model.id).modal("hide");
+    //   }
+    // });
   },
-  
+
   deleteCard: function(event) {
     event.preventDefault();
     event.stopImmediatePropagation();
-    
+
     cardView = this;
     this.model.destroy({
       success: function(model, response, options) {
@@ -65,11 +67,11 @@ TrelloClone.Views.ShowCard = Backbone.View.extend({
       }
     });
   },
-  
+
   cardReorder: function(event, index) {
     this.$el.trigger("update-list", [this.model, index]);
   },
-  
+
   getCard: function(event) {
     return this.model;
   }
